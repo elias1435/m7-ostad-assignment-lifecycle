5db9c763858a027083ac4386394b971c1b6ab5c2
elias1435 <elias1435@gmail.com>
2023-08-14 20:41:23 +0600

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,7 @@
+<script></script>
+<template>
+    <div class="p-1">
+        <h1 class="text-xl text-center">Welcome</h1>
+        <h1 class="text-xl text-center">Navigate to the Carousel and Chart Pages from the left sidebar</h1>
+    </div>
+</template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+<template>
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+   <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+      <ul class="space-y-2 font-medium">
+        <li>
+           <RouterLink to="/" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <span class="flex-1 ml-3 whitespace-nowrap">Home</span>
+           </RouterLink>
+        </li>
+        <li>
+           <RouterLink to="/carousel" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <span class="flex-1 ml-3 whitespace-nowrap">Carousel</span>
+           </RouterLink>
+        </li>
+         <li>
+            <RouterLink to="/chart" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <span class="ml-3">Charts</span>
+            </RouterLink>
+         </li>
+      </ul>
+   </div>
+</aside>
+</template>
